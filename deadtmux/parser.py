a0ebea64551a6e18765e3c2781495a144ef8f8cd
esehara <esehara@gmail.com>
2012-12-21 22:55:52 +0900

    Start: First Commit

---
+++ deadtmux/parser.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+class YamlManagement(object):
+
+    def __init__(self, yaml_string):
+        pass
+
+if __name__ == "__main__":
+    global debug
+    debug = True
---
+++ deadtmux/util.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
+def debug_print(print_string, debug=False):
+    if debug is True:
+        print(print_string)
