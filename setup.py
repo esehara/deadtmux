392764a86e3dbbf511af3971a37608c5f6151a15
esehara <esehara@gmail.com>
2012-12-22 18:19:26 +0900

    FIX: setup file

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
+import sys
 from setuptools import setup
+
+version = sys.version_info[:2]
+install_requires = ['PyYAML', ]
+
+if version < (2, 7) or (3, 0) <= version <= (3, 1):
+    install_requires += ['argparse']
 
 setup(
     name="deadtmux",
@@ -9,5 +16,5 @@ setup(
     author="shigeo esehara",
     author_email="esehara@gmail.com",
     packages=['deadtmux', ],
-    entry_points={'console_scripts': 'deadtmux=deadtmux:commands'},
-    install_requires=['PyYAML', ])
+    entry_points={'console_scripts': 'deadtmux=deadtmux.console:main'},
+    install_requires=install_requires)
